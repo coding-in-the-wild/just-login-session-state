2e51c63cbc9d8b210c59e1d50c429862a370989b
Joseph Dykstra <josephdykstra@gmail.com>
2015-05-11 08:36:39 -0500

    rename keys

--- test/testRespectsLevelLock.js
+++ test/testRespectsLevelLock.js
@@ -5,23 +5,23 @@ var lock = require('level-lock')
 var init = require('./helpers/init.js')
 
 test('isAuthenticated respects level-lock locks', function(t) {
-	var jlc = JustLoginCore(new Levelup()) //, jlcOpts)
+	var jlc = JustLoginCore(new Levelup())
 	var sdb = init(jlc)
 
 	t.plan(7)
 
-	jlc.isAuthenticated('token1', function (err, val) {
+	jlc.isAuthenticated('session1', function (err, val) {
 		t.notOk(err, 'first auth has no err' + (err && err.message))
 		t.notOk(val, 'unauthenticated')
 
-		var unlock = lock(sdb, 'token2', 'rw')
+		var unlock = lock(sdb, 'session2', 'rw')
 		t.ok(unlock, 'lock aquired')
-		jlc.isAuthenticated('token2', function (err, val) {
+		jlc.isAuthenticated('session2', function (err, val) {
 			t.ok(err, 'an error happened when locked ' + (err && err.message))
 			t.notOk(val, 'unauthenticated')
 
 			unlock && unlock()
-			jlc.isAuthenticated('token2', function (err, val) {
+			jlc.isAuthenticated('session2', function (err, val) {
 				t.notOk(err, 'no error after lock given up ' + (err && err.message))
 				t.notOk(val, 'unauthenticated')
 
@@ -33,21 +33,21 @@ test('isAuthenticated respects level-lock locks', function(t) {
 
 
 test('unauthenticate respects level-lock locks', function(t) {
-	var jlc = JustLoginCore(new Levelup()) //, jlcOpts)
+	var jlc = JustLoginCore(new Levelup())
 	var sdb = init(jlc)
 
 	t.plan(4)
 
-	jlc.unauthenticate('token1', function (err) {
+	jlc.unauthenticate('session1', function (err) {
 		t.notOk(err, 'first auth has no err' + (err && err.message))
 
-		var unlock = lock(sdb, 'token2', 'rw')
+		var unlock = lock(sdb, 'session2', 'rw')
 		t.ok(unlock, 'lock aquired')
-		jlc.unauthenticate('token2', function (err) {
+		jlc.unauthenticate('session2', function (err) {
 			t.ok(err, 'an error happened when locked ' + (err && err.message))
 
 			unlock && unlock()
-			jlc.unauthenticate('token2', function (err) {
+			jlc.unauthenticate('session2', function (err) {
 				t.notOk(err, 'no error after lock given up ' + (err && err.message))
 
 				t.end()

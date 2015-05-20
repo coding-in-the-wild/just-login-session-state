2443635bab92c499f047a192be223508a8b20617
Joseph Dykstra <josephdykstra@gmail.com>
2015-05-19 21:07:38 -0500

    added createSession.js() and continueSession.js() moved authenticate.js, isAuthenticated.js, unauthenticate.js to ./lib/ added lib/createSession.js, lib/continueSession.js removed xtend dep, added random-uuid-v4 dep

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var xtend = require('xtend')
 var Expirer = require('expire-unused-keys')
 var spaces = require('level-spaces')
-var unauthenticate = require('./unauthenticate.js')
-var isAuthenticated = require('./isAuthenticated.js')
-var authenticate = require('./authenticate.js')
+var authenticate = require('./lib/authenticate.js')
+var createSession = require('./lib/createSession.js')
+var continueSession = require('./lib/continueSession.js')
+var unauthenticate = require('./lib/unauthenticate.js')
+var isAuthenticated = require('./lib/isAuthenticated.js')
 
-var defaultOptions = {
-	sessionUnauthenticatedAfterMsInactivity: 7 * 24 * 60 * 60 * 1000, // 1 week
-	sessionTimeoutCheckIntervalMs: 10 * 1000 // 10 sec
-}
+var defaultInactivity = 7 * 24 * 60 * 60 * 1000 // 1 week
+var defaultInterval = 10 * 1000 // 10 sec
 
 module.exports = function sessionState(core, db, opts) {
 	if (!core) throw new Error('Expected a just-login-core instance')
 	if (!db) throw new Error('Expected a levelup database')
-	var options = xtend(defaultOptions, opts)
+	if (!opts) opts = {}
 
 	var expirer = new Expirer(
-		options.sessionUnauthenticatedAfterMsInactivity,
+		opts.sessionUnauthenticatedAfterMsInactivity || defaultInactivity,
 		spaces(db, 'session-expiration'),
-		options.sessionTimeoutCheckIntervalMs
+		opts.sessionTimeoutCheckIntervalMs || defaultInterval
 	)
 	var authedSessionsDb = spaces(db, 'session')
 
 	core.on('authenticated', authenticate(authedSessionsDb, expirer))
-
+	core.createSession = createSession(authedSessionsDb, expirer)
+	core.continueSession = continueSession(authedSessionsDb, expirer)
 	core.unauthenticate = unauthenticate(authedSessionsDb, expirer)
 	core.isAuthenticated = isAuthenticated(authedSessionsDb, expirer)
 
